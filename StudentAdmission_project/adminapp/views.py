430b5fbaf5cf1062afa33e73f41ff2d270957f91
NOORA AULIA HIDAYAT <73746751+noora20FH@users.noreply.github.com>
2023-05-24 23:37:46 +0700

    Django installation for backend

---
+++ StudentAdmission_project/adminapp/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def say_hello(request): 
+    # return HttpResponse('Hello Noora!')
+    # instead of returning http response, we use render to render http markup in templates folder
+    return render(request, 'hello.html', {'name': 'Noora'})
